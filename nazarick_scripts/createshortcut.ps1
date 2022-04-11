08ee7bd7aeac0773dfb9ab2f29d6abbc878a0782
Matt G <matt.0609@hotmail.com>
2022-04-10 19:34:54 -0700

    Fix shortcut arguments

--- nazarick_scripts/createshortcut.ps1
+++ nazarick_scripts/createshortcut.ps1
@@ -3,5 +3,5 @@ $WshShell = New-Object -comObject WScript.Shell
 
 $Shortcut = $WshShell.CreateShortcut("$DesktopPath\Valheim - Nazarick.lnk")
 $Shortcut.TargetPath = "powershell.exe"
-$Shortcut.Arguments = "-ExecutionPolicy Bypass -File '$PSScriptRoot/updater.ps1'"
+$Shortcut.Arguments = "-ExecutionPolicy Bypass -File $PSScriptRoot/updater.ps1"
 $Shortcut.Save()
