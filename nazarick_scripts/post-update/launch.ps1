25b55c2d6d02dae04bc19356272be3502bd753cf
Matt G <matt.0609@hotmail.com>
2022-04-23 23:44:28 -0700

    script: Modify launch.ps1
    Vulkan is now forced on launch.

--- nazarick_scripts/post-update/launch.ps1
+++ nazarick_scripts/post-update/launch.ps1
@@ -1,4 +1,4 @@
 # Launch game
 Write-Output "`nLaunching Valheim...`n"
-Start-Process -FilePath "valheim.exe"
+Start-Process -FilePath "valheim.exe" -ArgumentList "-force-vulkan"
 Write-Output "`nValheim Launched.`n"
