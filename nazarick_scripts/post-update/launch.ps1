ef63ee8cfdd7b889254474061dfc62bd32a2f356
Matt G <matt.0609@hotmail.com>
2022-04-26 05:47:48 -0700

    scripts: Modify launch.ps1
    launch.ps1 now ensures that Steam is currently running before
    launching the game.

--- nazarick_scripts/post-update/launch.ps1
+++ nazarick_scripts/post-update/launch.ps1
@@ -1,3 +1,16 @@
+$steam = Get-Process steam -ErrorAction SilentlyContinue
+
+while(!$steam) {
+	# Alert user that steam is not launched
+	Write-Output "`nThis game requires Steam to be running."
+
+	# Prevent window closing
+	Read-Host -Prompt "Press Enter once Steam is running. "
+
+	# Refresh variable
+	$steam = Get-Process steam -ErrorAction SilentlyContinue
+}
+
 # Launch game
 Write-Output "`nLaunching Valheim...`n"
 Start-Process -FilePath "valheim.exe"
