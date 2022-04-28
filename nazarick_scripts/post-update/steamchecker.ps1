c996f7a2822e274c747ccedb66d3263177467679
Matt G <matt.0609@hotmail.com>
2022-04-27 18:04:59 -0700

    scripts: Modify steamchecker.ps1
    Mostly just added a 'Steam is running' output and modified the
    formatting.

--- nazarick_scripts/post-update/steamchecker.ps1
+++ nazarick_scripts/post-update/steamchecker.ps1
@@ -1,13 +1,17 @@
-Write-Output "`n[ Checking if Steam is running... ]"
+Write-Output "`n[ Checking if Steam is running... ]`n"
 $steam = Get-Process steam -ErrorAction SilentlyContinue
 
 while(!$steam) {
 	# Alert user that steam is not launched
-	Write-Output "`nThis game requires Steam to be running."
+	Write-Output "This game requires Steam to be running."
 
 	# Prevent window closing
-	Read-Host -Prompt "Press Enter once Steam is running.    "
+	Read-Host -Prompt "Press Enter once Steam is running.`n"
 
 	# Refresh variable
 	$steam = Get-Process steam -ErrorAction SilentlyContinue
+}
+
+if ($steam) {
+	Write-Output "Steam is running."
 }
